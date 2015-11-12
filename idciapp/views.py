ffb59cd9526ad954f68f4fad69cd1cbf7297c46b
baladewa88 <sutadi.triputra@gmail.com>
2015-11-12 10:27:16 +0700

    Koneksi URL Index ke Template

--- idciapp/views.py
+++ idciapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.template import RequestContext
 
 # Create your views here.
 
+def index(request):
+    return render(request, 'idciapp/index.html', {})    
