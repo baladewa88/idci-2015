5db9e6398ee4c38d7aab0fd3a766a943d07b3b28
baladewa88 <sutadi.triputra@gmail.com>
2015-11-18 10:35:12 +0700

    Implement Bootsstrap

--- idciapp/views.py
+++ idciapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Papers
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'idciapp/index.html', {})    
+    paperList = Papers.objects.all()[:5]
+    popularPaperList = Papers.objects.order_by('-ncites')[:5]
+    return render(request, 'idciapp/index.html', {'paper':paperList, 'popular_paper':popularPaperList})
+
+def popularPaper(request):
+    popularPaperList = Papers.objects.order_by('ncites')[:5]
+    return render(request, 'idciapp/index.html', {'popular_paper':popularPaperList})
